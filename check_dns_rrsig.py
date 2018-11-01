fbfcf4e1929d813eb2714060d6af515e2986f9fd
Ana C. Custura <ana@netstat.org.uk>
2018-11-01 14:44:13 +0000

    Very simple plugin for checking RRSIG expiry on DNSSEC enabled domains

---
+++ check_dns_rrsig.py
@@ -0,0 +1,41 @@
+import subprocess
+import argparse
+import sys
+from datetime import datetime
+
+def check_expiry(domain,c,w):
+    OK, WARNING, CRITICAL, UNKNOWN = range(4)
+    t = None
+    
+    p = subprocess.Popen(['dig', domain,  '+dnssec', '+short', 'SOA'], stdout=subprocess.PIPE)
+    res = p.communicate()[0]
+    res = (res.decode("utf-8")).split('\n')
+    for line in res: 
+        if line.startswith('SOA'):
+            t = line.split(' ')[4]
+    if t == None:
+        print('Zone does not exist or is not DNSSEC enabled')
+        return UNKNOWN
+    else:
+        d = datetime.strptime(t,'%Y%m%d%H%M%S')
+        present = datetime.now()
+        delta = d - present
+     
+        print('Signature expires in %s days' % str(delta.days))
+
+        if delta.days < c:
+            return CRITICAL
+        if delta.days < w:
+            return WARNING
+        else: return OK 
+   
+
+parser = argparse.ArgumentParser(description='Checks the RRSIG expiry date on the SOA record of a DNSSEC enabled domain')
+
+parser.add_argument('-c', type=int, help='Number of days left until expiry for which the plugin returns CRITICAL', default=10)
+parser.add_argument('-w', type=int, help='Number of days left until expiry for which the plugin returns WARNING', default=28)
+parser.add_argument('domain', type=str, help='Domain to check')
+
+args = parser.parse_args()
+
+sys.exit(check_expiry(args.domain, args.c, args.w))
